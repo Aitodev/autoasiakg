9ab71f4c34b84d68a208524605598d8e573c6c5f
2023025intelli <surmali_yusupov@mail.ru>
2020-10-29 05:08:04 +0600

    trim spaces

--- uploader/admin.py
+++ uploader/admin.py
@@ -39,8 +39,8 @@ class CsvUploadAdmin(admin.ModelAdmin):
                             sh = wb.sheet_by_index(0)
                             keys = {}
                             for i in range(len(sh.row(0))):
-                                if str(sh.row(0)[i].value).lower() not in keys:
-                                    keys[str(sh.row(0)[i].value).lower()] = i
+                                if str(sh.row(0)[i].value).strip().lower() not in keys:
+                                    keys[str(sh.row(0)[i].value).strip().lower()] = i
                             for i in range(1, sh.nrows):
                                 if 'сategory' in keys and sh.row(i)[keys['сategory']].value:
                                     subcategory = None
@@ -50,49 +50,57 @@ class CsvUploadAdmin(admin.ModelAdmin):
                                     automodel1 = None
                                     automodel2 = None
                                     manufacturer = None
-                                    if Category.objects.filter(name=sh.row(i)[keys['сategory']].value).exists():
-                                        category = Category.objects.filter(name=sh.row(i)[keys['сategory']].value).first()
+                                    category_str = str(sh.row(i)[keys['сategory']].value).strip()
+                                    if Category.objects.filter(name=category_str).exists():
+                                        category = Category.objects.filter(name=category_str).first()
                                     else:
-                                        category = Category.objects.create(name=sh.row(i)[keys['сategory']].value)
+                                        category = Category.objects.create(name=category_str)
                                     if 'subcategory1' in keys and sh.row(i)[keys['subcategory1']].value and category:
-                                        if Subcategory.objects.filter(name=sh.row(i)[keys['subcategory1']].value).exists():
-                                            subcategory = Subcategory.objects.filter(name=sh.row(i)[keys['subcategory1']].value).first()
+                                        subcategory_str = str(sh.row(i)[keys['subcategory1']].value).strip()
+                                        if Subcategory.objects.filter(name=subcategory_str).exists():
+                                            subcategory = Subcategory.objects.filter(name=subcategory_str).first()
                                         else:
-                                            subcategory = Subcategory.objects.create(name=sh.row(i)[keys['subcategory1']].value, category=category)
+                                            subcategory = Subcategory.objects.create(name=subcategory_str, category=category)
                                     if 'subcategory2' in keys and sh.row(i)[keys['subcategory2']].value and Subcategory:
-                                        if Subcategory1.objects.filter(name=sh.row(i)[keys['subcategory2']].value).exists():
-                                            subcategory1 = Subcategory1.objects.filter(name=sh.row(i)[keys['subcategory2']].value).first()
+                                        subcategory1_str = str(sh.row(i)[keys['subcategory2']].value).strip()
+                                        if Subcategory1.objects.filter(name=subcategory1_str).exists():
+                                            subcategory1 = Subcategory1.objects.filter(name=subcategory1_str).first()
                                         else:
-                                            subcategory1 = Subcategory1.objects.create(name=sh.row(i)[keys['subcategory2']].value, subcategory=subcategory)
+                                            subcategory1 = Subcategory1.objects.create(name=subcategory1_str, subcategory=subcategory)
                                     if 'autobrand' in keys and sh.row(i)[keys['autobrand']].value:
-                                        if Brand.objects.filter(name=sh.row(i)[keys['autobrand']].value).exists():
-                                            brand = Brand.objects.filter(name=sh.row(i)[keys['autobrand']].value).first()
+                                        brand_str = str(sh.row(i)[keys['autobrand']].value).strip()
+                                        if Brand.objects.filter(name=brand_str).exists():
+                                            brand = Brand.objects.filter(name=brand_str).first()
                                         else:
-                                            brand = Brand.objects.create(name=sh.row(i)[keys['autobrand']].value)
+                                            brand = Brand.objects.create(name=brand_str)
                                     if 'automodel1' in keys and sh.row(i)[keys['automodel1']].value and brand:
-                                        if Automodel.objects.filter(name=sh.row(i)[keys['automodel1']].value).exists():
-                                            automodel = Automodel.objects.filter(name=sh.row(i)[keys['automodel1']].value).first()
+                                        automodel_str = str(sh.row(i)[keys['automodel1']].value).strip()
+                                        if Automodel.objects.filter(name=automodel_str).exists():
+                                            automodel = Automodel.objects.filter(name=automodel_str).first()
                                         else:
-                                            automodel = Automodel.objects.create(name=sh.row(i)[keys['automodel1']].value, brand=brand)
+                                            automodel = Automodel.objects.create(name=automodel_str, brand=brand)
                                     if 'automodel2' in keys and sh.row(i)[keys['automodel2']].value and automodel:
-                                        if Automodel1.objects.filter(name=sh.row(i)[keys['automodel2']].value).exists():
-                                            automodel1 = Automodel1.objects.filter(name=sh.row(i)[keys['automodel2']].value).first()
+                                        automodel1_str = str(sh.row(i)[keys['automodel2']].value).strip()
+                                        if Automodel1.objects.filter(name=automodel1_str).exists():
+                                            automodel1 = Automodel1.objects.filter(name=automodel1_str).first()
                                         else:
-                                            automodel1 = Automodel1.objects.create(name=sh.row(i)[keys['automodel2']].value, automodel=automodel)
+                                            automodel1 = Automodel1.objects.create(name=automodel1_str, automodel=automodel)
                                     if 'automodel3' in keys and sh.row(i)[keys['automodel3']].value and automodel1:
-                                        if Automodel2.objects.filter(name=sh.row(i)[keys['automodel3']].value).exists():
-                                            automodel2 = Automodel2.objects.filter(name=sh.row(i)[keys['automodel3']].value).first()
+                                        automodel2_str = str(sh.row(i)[keys['automodel3']].value).strip()
+                                        if Automodel2.objects.filter(name=automodel2_str).exists():
+                                            automodel2 = Automodel2.objects.filter(name=automodel2_str).first()
                                         else:
-                                            automodel2 = Automodel2.objects.create(name=sh.row(i)[keys['automodel3']].value, automodel1=automodel1)
+                                            automodel2 = Automodel2.objects.create(name=automodel2_str, automodel1=automodel1)
                                     if 'manufacturer' in keys and sh.row(i)[keys['manufacturer']].value:
-                                        if Manufacturer.objects.filter(name=sh.row(i)[keys['manufacturer']].value).exists():
-                                            manufacturer = Manufacturer.objects.filter(name=sh.row(i)[keys['manufacturer']].value).first()
+                                        manufacturer_str = str(sh.row(i)[keys['manufacturer']].value).strip()
+                                        if Manufacturer.objects.filter(name=manufacturer_str).exists():
+                                            manufacturer = Manufacturer.objects.filter(name=manufacturer_str).first()
                                         else:
-                                            manufacturer = Manufacturer.objects.create(name=sh.row(i)[keys['manufacturer']].value)
-                                    code = sh.row(i)[keys['productcode']].value if 'productcode' in keys else None
-                                    name = sh.row(i)[keys['productname']].value if 'productname' in keys else None
-                                    description = sh.row(i)[keys['shortdescription']].value if 'shortdescription' in keys else None
-                                    price = sh.row(i)[keys['price']].value if 'price' in keys else None
+                                            manufacturer = Manufacturer.objects.create(name=manufacturer_str)
+                                    code = str(sh.row(i)[keys['productcode']].value).strip() if 'productcode' in keys else None
+                                    name = str(sh.row(i)[keys['productname']].value).strip() if 'productname' in keys else None
+                                    description = str(sh.row(i)[keys['shortdescription']].value).strip() if 'shortdescription' in keys else None
+                                    price = str(sh.row(i)[keys['price']].value).strip() if 'price' in keys else None
                                     if name:
                                         Product.objects.create(name=name, description=description, product_code=code, category=category, price=price,
                                                                manufacturer=manufacturer, subcategory=subcategory, subcategory1=subcategory1,
@@ -110,49 +118,49 @@ class CsvUploadAdmin(admin.ModelAdmin):
                             automodel1 = None
                             automodel2 = None
                             manufacturer = None
-                            if Category.objects.filter(name=row['Сategory']).exists():
-                                category = Category.objects.filter(name=row['Сategory']).first()
+                            if Category.objects.filter(name=str(row['Сategory']).strip()).exists():
+                                category = Category.objects.filter(name=str(row['Сategory']).strip()).first()
                             else:
-                                category = Category.objects.create(name=row['Сategory'])
+                                category = Category.objects.create(name=str(row['Сategory']).strip())
                             if row['Subcategory1']:
-                                if Subcategory.objects.filter(name=row['Subcategory1']).exists():
-                                    subcategory = Subcategory.objects.filter(name=row['Subcategory1']).first()
+                                if Subcategory.objects.filter(name=str(row['Subcategory1']).strip()).exists():
+                                    subcategory = Subcategory.objects.filter(name=str(row['Subcategory1']).strip()).first()
                                 else:
-                                    subcategory = Subcategory.objects.create(name=row['Subcategory1'], category=category)
+                                    subcategory = Subcategory.objects.create(name=str(row['Subcategory1']).strip(), category=category)
                             if row['Subcategory2']:
-                                if Subcategory1.objects.filter(name=row['Subcategory2']).exists():
-                                    subcategory1 = Subcategory1.objects.filter(name=row['Subcategory2']).first()
+                                if Subcategory1.objects.filter(name=str(row['Subcategory2']).strip()).exists():
+                                    subcategory1 = Subcategory1.objects.filter(name=str(row['Subcategory2']).strip()).first()
                                 else:
-                                    subcategory1 = Subcategory1.objects.create(name=row['Subcategory2'], subcategory=subcategory)
+                                    subcategory1 = Subcategory1.objects.create(name=str(row['Subcategory2']).strip(), subcategory=subcategory)
                             if row['Autobrand']:
-                                if Brand.objects.filter(name=row['Aufobrand']).exists():
-                                    brand = Brand.objects.filter(name=row['Aufobrand']).first()
+                                if Brand.objects.filter(name=str(row['Aufobrand']).strip()).exists():
+                                    brand = Brand.objects.filter(name=str(row['Aufobrand']).strip()).first()
                                 else:
-                                    brand = Brand.objects.create(name=row['Aufobrand'])
+                                    brand = Brand.objects.create(name=str(row['Aufobrand']).strip())
                             if row['Automodel1']:
-                                if Automodel.objects.filter(name=row['Automodel1']).exists():
-                                    automodel = Automodel.objects.filter(name=row['Automodel1']).first()
+                                if Automodel.objects.filter(name=str(row['Automodel1']).strip()).exists():
+                                    automodel = Automodel.objects.filter(name=str(row['Automodel1']).strip()).first()
                                 else:
-                                    automodel = Automodel.objects.create(name=row['Automodel1'], brand=brand)
+                                    automodel = Automodel.objects.create(name=str(row['Automodel1']).strip(), brand=brand)
                             if row['Automodel2']:
-                                if Automodel1.objects.filter(name=row['Automodel2']).exists():
-                                    automodel1 = Automodel1.objects.filter(name=row['Automodel2']).first()
+                                if Automodel1.objects.filter(name=str(row['Automodel2']).strip()).exists():
+                                    automodel1 = Automodel1.objects.filter(name=str(row['Automodel2']).strip()).first()
                                 else:
-                                    automodel1 = Automodel1.objects.create(name=row['Automodel2'], automodel=automodel)
+                                    automodel1 = Automodel1.objects.create(name=str(row['Automodel2']).strip(), automodel=automodel)
                             if row['Automodel3']:
-                                if Automodel2.objects.filter(name=row['Automodel3']).exists():
-                                    automodel2 = Automodel2.objects.filter(name=row['Automodel3']).first()
+                                if Automodel2.objects.filter(name=str(row['Automodel3']).strip()).exists():
+                                    automodel2 = Automodel2.objects.filter(name=str(row['Automodel3']).strip()).first()
                                 else:
-                                    automodel2 = Automodel2.objects.create(name=row['Automodel3'], automodel1=automodel1)
+                                    automodel2 = Automodel2.objects.create(name=str(row['Automodel3']).strip(), automodel1=automodel1)
                             if row['manufacturer']:
-                                if Manufacturer.objects.filter(name=row['manufacturer']).exists():
-                                    manufacturer = Manufacturer.objects.filter(name=row['manufacturer']).first()
+                                if Manufacturer.objects.filter(name=str(row['manufacturer']).strip()).exists():
+                                    manufacturer = Manufacturer.objects.filter(name=str(row['manufacturer']).strip()).first()
                                 else:
-                                    manufacturer = Manufacturer.objects.create(name=row['manufacturer'])
-                            code = row['Productcode']
-                            name = row['Productname']
-                            description = row['Shortdescription']
-                            price = Decimal(row['Price'].replace(',', ''))
+                                    manufacturer = Manufacturer.objects.create(name=str(row['manufacturer']).strip())
+                            code = str(row['Productcode']).strip()
+                            name = str(row['Productname']).strip()
+                            description = str(row['Shortdescription']).strip()
+                            price = Decimal(row['Price'].replace(',', '').strip())
                             if name:
                                 Product.objects.create(name=name, description=description, product_code=code, category=category, price=price,
                                                        manufacturer=manufacturer, subcategory=subcategory, subcategory1=subcategory1,
